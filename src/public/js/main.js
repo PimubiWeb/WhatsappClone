a3ce03c69acee2f5af43365fa7f5377afb635569
PabloPC <pimubiweb@gmail.com>
2022-03-09 00:39:26 +0100

    login 1.0

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,21 +2,47 @@ $(function() {
 
     const socket = io(); //conexion web socket
 
-    // obtener elementos del DOM de mi html
+    // obtener elementos del DOM de mi sala de chat
 
     const $messageForm = $('#message-form');
     const $messageBox = $('#message');
     const $chat = $('#chat');
     
-    // eventos
+    // obtener elementos del DOM de mi login de usuario
+    const $nickForm = $('#nickForm');
+    const $nickError = $('#nickError');
+    const $nickname = $('#nickname');
+
+    const $users = $('usernames');
+
+    // evento de registrar un nuevo usuario
+    $nickForm.submit( e => {
+        e.preventDefault();
+        socket.emit('new user', $nickname.val(), data => {
+            if(data) {
+                $('#loginChat').hide();
+                $('#contentChat').show();
+            }else {
+                $nickError.html(`
+                    <div class="alert alert-danger">
+                        Username already exits.
+                    </div>
+                `);
+            }
+            $nickname.val('');
+        });
+    })
+
+    // evento de enviar un mensaje siendo ya un usuario
     $messageForm.submit( e => {
         e.preventDefault();
         socket.emit('send message', $messageBox.val());
         $messageBox.val('');
     })
 
+    //new message es el evento enviado desde socket.js que envia la data, es decir, el mensaje que envia un usuario
     socket.on('new message', function(data) {
-        $chat.append(data + '<br/>');
+        $chat.append(data + '<br/>'); //metemos en la sala del chat
     })
 
 });
--- src/sockets.js
+++ src/sockets.js
@@ -1,14 +1,35 @@
 
 module.exports = function (io) {
 
+    let nicknames = [
+        'pablo',
+        'jose',
+        'fer',
+        'rut'
+    ];
+
     io.on('connection', socket => {
-        console.log('new user coneccted');
+        console.log('new user conected');
+        
+        //socket recoge evento new user del main.js
+        socket.on('new user', (data, callback) => {
+            console.log(data)
 
+            //si no esta en el array el indice sera -1, por lo tanto, si exite el usuario vale n y sino -1
+            if(nicknames.indexOf(data) !=-1) {
+                callback(false);
+            }else{
+                callback(true);
+                socket.nickname = data;
+                nicknames.push(socket.nickname);
 
-        socket.on('send message', function(data) { //recoge los mensajes
-            io.sockets.emit('new message', data); //devuelve el servidor los mensajes
-            
-        })
+            }
+        });
+
+        //socket recoge evento send mensage del main.js
+        socket.on('send message', function(data) { //data sera el valor pasado por $messageBox.val() del main.js
+            io.sockets.emit('new message', data);
+        });
     });
 
 }
